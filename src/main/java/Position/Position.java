b5659c58237f00cfd13547369883ac1902011f14
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-06-23 22:16:22 +0300

    архитектура до рефакторинга

--- src/main/java/Position/Position.java
+++ src/main/java/Position/Position.java
@@ -1,5 +1,7 @@
 package Position;
 
+import java.util.Objects;
+
 public class Position {
     private int x;
     private int y;
@@ -29,8 +31,15 @@ public class Position {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
 
-
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
